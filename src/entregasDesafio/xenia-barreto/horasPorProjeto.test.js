302216c079ef543793d115620b452d469fe969c9
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-08-20 14:29:12 -0300

    testando todas funcionalidades

--- src/entregasDesafio/xenia-barreto/horasPorProjeto.test.js
+++ src/entregasDesafio/xenia-barreto/horasPorProjeto.test.js
@@ -1,18 +1,45 @@
-const { calcularHorasDeProjeto } = require("../../dominio/calculadora/Projeto/horasPorProjeto")
+const {
+  calcularHorasDeProjeto,
+} = require("../../dominio/calculadora/Projeto/horasPorProjeto");
 
 describe("Calculo horas trabalhadas para determinadas funcionalidades", () => {
-    test("Cálculo 3 funcionalidades da lista", () => {
-        const listaFuncionalidadesTrabalhadas = ["responsividade", "otimizacao_seo", "ssr"]
-        const horasParaCriacaoAlgumasFuncionalidades = calcularHorasDeProjeto(listaFuncionalidadesTrabalhadas)
+  test("Cálculo 3 funcionalidades da lista", () => {
+    const listaFuncionalidadesTrabalhadas = [
+      "responsividade",
+      "otimizacao_seo",
+      "ssr",
+    ];
+    const horasParaCriacaoAlgumasFuncionalidades = calcularHorasDeProjeto(
+      listaFuncionalidadesTrabalhadas
+    );
 
-        expect(horasParaCriacaoAlgumasFuncionalidades).toBe(40)
+    expect(horasParaCriacaoAlgumasFuncionalidades).toBe(40);
+  });
+
+  test("Calcular horas de duas outras funcionalidades", () => {
+    const outrasDuasFuncionalidades = ["formulario", "integracao_api_propria"];
+    const horasParaCriacaoMaisDuasFuncionalidades = calcularHorasDeProjeto(
+      outrasDuasFuncionalidades
+    );
+
+    expect(horasParaCriacaoMaisDuasFuncionalidades).toBe(32);
+  });
+
+  test("Calcular todas as horas de todas funcionalidades", () => {
+    const calculoTodasFuncionalidades = [
+        "responsividade",
+        "otimizacao_seo",
+        "ssr",
+        "formulario",
+        "integracao_api_propria",
+        "setup",
+        "integracao_mailchimp",
+        "construcao_1_pagina",
+    ];
+    const tempoTotalFuncionalidades = calcularHorasDeProjeto(
+        calculoTodasFuncionalidades
+    );
+    expect(tempoTotalFuncionalidades).toBe(104);
     })
+});
 
-    test("Calcular horas de duas outras funcionalidades", () => {
-        const outrasDuasFuncionalidades = ["formulario","integracao_api_propria"]
-        const horasParaCriacaoMaisDuasFuncionalidades = calcularHorasDeProjeto(outrasDuasFuncionalidades)
-
-        expect(horasParaCriacaoMaisDuasFuncionalidades).toBe(32)
-    })
-
-})
