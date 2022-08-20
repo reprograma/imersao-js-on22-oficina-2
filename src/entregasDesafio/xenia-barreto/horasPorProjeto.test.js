acf2d15262abf8b168333f59b61cb0d5cb3e51ac
Xênia Barreto <xenia.barreto@queimadiaria.com.br>
2022-08-20 13:55:55 -0300

    testando função horas por projeto

--- src/entregasDesafio/xenia-barreto/horasPorProjeto.test.js
+++ src/entregasDesafio/xenia-barreto/horasPorProjeto.test.js
@@ -1,1 +1,10 @@
 const { calcularHorasDeProjeto } = require("../../dominio/calculadora/Projeto/horasPorProjeto")
+
+describe("Calculo horas trabalhadas para determinadas funcionalidades", () => {
+    test("Cálculo 3 funcionalidades da lista", () => {
+        const listaFuncionalidadesTrabalhadas = ["responsividade", "otimizacao_seo", "ssr"]
+        const horasParaCriacaoAlgumasFuncionalidades = calcularHorasDeProjeto(listaFuncionalidadesTrabalhadas)
+
+        expect(horasParaCriacaoAlgumasFuncionalidades).toBe(40)
+    })
+})
