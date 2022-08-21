ff1216caefe60706fe3443c14a840f39a9e54c85
Barbara Pereira <barbara.pereira1990@gmail.com>
2022-08-21 17:45:30 -0300

    removendo test.only

--- src/dominio/calculadora/Hora/valorHora.test.js
+++ src/dominio/calculadora/Hora/valorHora.test.js
@@ -8,7 +8,7 @@ test("testa se 2 * 2 = 4", () => {
     expect(2 * 2).toBe(5)
 }) 
 
-test.only("Qual valor hora, de um salário de 1500 ", () => {
+test("Qual valor hora, de um salário de 1500 ", () => {
     const salario = 1500
     const valorHora = calcularValorPorHora(salario)
     console.log(valorHora)
